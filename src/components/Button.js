e8cf11a65c2d7f391614f0374544ce085ca3f35c
Md Abdullah Al Mamun <mamun.cseian@gmail.com>
2021-09-04 08:08:27 +0600

    completed

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,26 @@
 import React from "react";
 import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
 
 class Button extends React.Component {
-    static contextType = LanguageContext;
-    
+  renderSubmit(value) {
+    return value === "english" ? "Submit" : "Submit korun";
+  }
+
   render() {
-    const text = this.context === 'english' ? 'Submit' : 'sabmit';
-    return <button className="ui button primary">{text}</button>;
+    return (
+      <ColorContext.Consumer>
+      {(color) =>
+      
+      <button className={`ui button ${color}`}>
+          <LanguageContext.Consumer>
+            {(value) => this.renderSubmit(value)}
+          </LanguageContext.Consumer>
+        </button>
+      }
+        
+      </ColorContext.Consumer>
+    );
   }
 }
 
