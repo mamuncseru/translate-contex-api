37ed31572aafdad4054dec7bafaca7a29d63b543
Md Abdullah Al Mamun <mamun.cseian@gmail.com>
2021-09-05 06:34:25 +0600

    Created context store for understanding context

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,8 +3,8 @@ import LanguageContext from "../contexts/LanguageContext";
 import ColorContext from "../contexts/ColorContext";
 
 class Button extends React.Component {
-  renderSubmit(value) {
-    return value === "english" ? "Submit" : "Submit korun";
+  renderSubmit(language) {
+    return language === "english" ? "Submit" : "Submit korun";
   }
 
   render() {
@@ -14,7 +14,7 @@ class Button extends React.Component {
       
       <button className={`ui button ${color}`}>
           <LanguageContext.Consumer>
-            {(value) => this.renderSubmit(value)}
+            {({ language }) => this.renderSubmit(language)}
           </LanguageContext.Consumer>
         </button>
       }
--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
-const context = React.createContext('english');
+const Context = React.createContext('english');
 
-console.log(context);
+export class LanguageStore extends React.Component {
+    state = { Language: 'english' };
 
-export default context;
+    onLanguageChange = (language) => {
+
+        this.setState({ language });
+    }
+
+    render() {
+        return (
+            <Context.Provider value={{ ...this.state, onLanguageChange: this.onLanguageChange }}>
+                {this.props.children}
+            </Context.Provider>
+        )
+    }
+}
+
+export default Context;
